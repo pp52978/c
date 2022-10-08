4ba558207a8506eb9c6286f9335bc430a340919c
pp <631843571@qq.com>
2022-10-08 10:02:32 +0800

    test!

--- test.c
+++ test.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 #define a 5
 int main(){
-    int b=0;
-    for(int i=1;i<=100;i++)
-    for(int i=3;i<=4;i++)
-    {
-        b++;
-        int l;
+    int b[]={1,2};
+
+
     printf("%d\n",b);
     }
-}
